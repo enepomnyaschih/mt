f702290f24124676707d592adc302a79a79f735d
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2013-09-17 18:54:13 +0700

    Sample 4.

--- public/mt/data/tweet.js
+++ public/mt/data/tweet.js
@@ -7,6 +7,8 @@
 	this.time = config.time;
 	this.like = config.like;
 	this.retweet = config.retweet;
+	this.likeChangeEvent = new JW.Event();
+	this.retweetChangeEvent = new JW.Event();
 };
 
 JW.extend(mt.data.Tweet, JW.Class, {
@@ -18,7 +20,32 @@ JW.extend(mt.data.Tweet, JW.Class, {
 	number time;
 	boolean like;
 	boolean retweet;
+	JW.Event<JW.ValueEventParams<boolean>> likeChangeEvent;
+	JW.Event<JW.ValueEventParams<boolean>> retweetChangeEvent;
 	*/
+	
+	// override
+	destroy: function() {
+		this.retweetChangeEvent.destroy();
+		this.likeChangeEvent.destroy();
+		this._super();
+	},
+	
+	setLike: function(value) {
+		if (this.like === value) {
+			return;
+		}
+		this.like = value;
+		this.likeChangeEvent.trigger(new JW.ValueEventParams(this, value));
+	},
+	
+	setRetweet: function(value) {
+		if (this.retweet === value) {
+			return;
+		}
+		this.retweet = value;
+		this.retweetChangeEvent.trigger(new JW.ValueEventParams(this, value));
+	}
 });
 
 mt.data.Tweet.createByJson = function(json) {
