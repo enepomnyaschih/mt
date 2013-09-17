445848331906e5e4f143d1a8e00a7d44e89dad83
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2013-09-17 17:35:02 +0700

    Sample 3.

--- public/mt/data/data.js
+++ public/mt/data/data.js
@@ -1,10 +1,12 @@
 ﻿mt.Data = function() {
 	mt.Data._super.call(this);
+	this.profile = null;
 	this.tweets = new JW.Array();
 };
 
 JW.extend(mt.Data, JW.Class, {
 	/*
+	mt.data.Profile profile;
 	JW.AbstractArray<mt.data.Tweet> tweets;
 	*/
 	
@@ -17,7 +19,8 @@ JW.extend(mt.Data, JW.Class, {
 
 mt.Data.createByJson = function(json) {
 	var data = new mt.Data();
-	data.tweets.addAll(JW.Array.map(json, mt.data.Tweet.createByJson));
+	data.profile = mt.data.Profile.createByJson(json.profile);
+	data.tweets.addAll(JW.Array.map(json.tweets, mt.data.Tweet.createByJson));
 	return data;
 };
 
