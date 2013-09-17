529cc1e9c4779ac8dc3113293805e38d6717ac64
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2013-09-17 12:00:02 +0700

    Commit first example.

---
+++ public/boot.js
@@ -0,0 +1,18 @@
+﻿var data;
+var profileBox;
+
+$(function() {
+	data = mt.Data.createByJson({
+		"profile": {
+			"fullName": "Egor Nepomnyaschih",
+			"shortName": "enepomnyaschih",
+			"avatarUrl32": "backend/avatar-32.png",
+			"avatarUrl48": "backend/avatar-48.png",
+			"tweets": 380,
+			"following": 21,
+			"followers": 27
+		}
+	});
+	profileBox = new mt.ProfileBox(data);
+	profileBox.renderTo("#container");
+});
---
+++ public/mt/data/data.js
@@ -0,0 +1,18 @@
+﻿mt.Data = function() {
+	mt.Data._super.call(this);
+	this.profile = null;
+};
+
+JW.extend(mt.Data, JW.Class, {
+	/*
+	mt.data.Profile profile;
+	*/
+});
+
+mt.Data.createByJson = function(json) {
+	var data = new mt.Data();
+	data.profile = mt.data.Profile.createByJson(json.profile);
+	return data;
+};
+
+mt.data = {};
