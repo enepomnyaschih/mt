b12a3422c26edcf8b1e603c1207a96de19c17ff1
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2014-04-06 17:01:31 +0700

    Deploy samples for jWidget 0.8.

---
+++ 0.8/build/resources/mt/application/application.jw.html.js
@@ -0,0 +1,1 @@
+JW.UI.template(mt.Application, { main: '<div jwclass="mt-application"><div jwid="wrap"><div jwid="profile-box"></div><div jwid="tweets"></div><div class="clear"></div></div></div>' });
