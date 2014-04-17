16eda0429576f430e387c168b665186fa9446463
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2014-04-17 18:19:24 +0700

    Deploy

---
+++ 0.9.0-1/mt/data/tweet.js
@@ -0,0 +1,28 @@
+﻿mt.data.Tweet = function(config) {
+	mt.data.Tweet._super.call(this);
+	this.fullName = config.fullName;
+	this.shortName = config.shortName;
+	this.avatarUrl48 = config.avatarUrl48;
+	this.contentHtml = config.contentHtml;
+	this.time = config.time;
+	this.like = config.like;
+	this.retweet = config.retweet;
+};
+
+JW.extend(mt.data.Tweet, JW.Class, {
+	/*
+	string fullName;
+	string shortName;
+	string contentHtml;
+	string avatarUrl48;
+	number time;
+	boolean like;
+	boolean retweet;
+	*/
+});
+
+mt.data.Tweet.createByJson = function(json) {
+	return new mt.data.Tweet(JW.apply({}, json, {
+		time: new Date().getTime() - json.timeAgo
+	}));
+};
---
+++ 0.9.0-7/boot.js
@@ -0,0 +1,8 @@
+﻿var data;
+var application;
+
+$(function() {
+	data = mt.Data.createByJson(dataJson);
+	application = new mt.Application(data);
+	application.renderTo("body");
+});
