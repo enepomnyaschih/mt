7cbb2da6670ae5c00d2cf623524a408e847bb342
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2017-02-05 13:41:28 +0600

    Unified code formatting.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-﻿import Tweet from './data/Tweet';
-import TweetView from './tweetview/TweetView';
+﻿import Tweet from "./data/Tweet";
+import TweetView from "./tweetview/TweetView";
 
-require('./reset.css');
+require("./reset.css");
 
-const json = require<any>('./data.json');
+const json = require<any>("./data.json");
 let tweet: Tweet;
 let tweetView: TweetView;
 
 $(function() {
 	tweet = Tweet.createByJson(json);
 	tweetView = new TweetView(tweet);
-	tweetView.renderTo('body');
+	tweetView.renderTo("body");
 });
