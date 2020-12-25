aaf9b62c702b68530f3f35e11dab3509622214cc
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2020-12-25 17:22:47 +0100

    Updated to jWidget 2.3.

--- src/view/TweetFeed.ts
+++ src/view/TweetFeed.ts
@@ -1,5 +1,5 @@
+import BindableArray from "jwidget/BindableArray";
 import Component from "jwidget/Component";
-import List from "jwidget/List";
 import template from "jwidget/template";
 import Tweet from "../model/Tweet";
 import TweetView from "./TweetView";
@@ -13,6 +13,6 @@ export default class TweetFeed extends Component {
 
 	protected renderTweets() {
 		const tweetViews = this.tweets.map(tweet => new TweetView(tweet));
-		return this.own(new List(tweetViews)).ownItems();
+		return this.own(new BindableArray(tweetViews)).ownValues();
 	}
 }
