f3193bd787846110b3809308a0f07eddc44133e3
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2021-04-03 19:34:00 +0200

    Updated to jWidget 2.3.

--- src/model/ApplicationData.ts
+++ src/model/ApplicationData.ts
@@ -1,10 +1,11 @@
 import BindableArray from "jwidget/BindableArray";
+import IBindableArray from "jwidget/IBindableArray";
 import Profile from "./Profile";
 import Tweet from "./Tweet";
 
 export default class ApplicationData {
 
-	readonly tweets: BindableArray<Tweet>;
+	readonly tweets: IBindableArray<Tweet>;
 
 	constructor(readonly profile: Profile, tweets: Tweet[] = []) {
 		this.tweets = new BindableArray(tweets);
